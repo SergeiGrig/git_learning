9c51e806b19d6a2cb175078fd8f36ca16971ea34
SergeiGrig <grigorevsp@mail.ru>
2022-07-19 17:39:09 +0300

    Удалена строка text Добавлена функция mul(a, b)

--- first/nums.py
+++ first/nums.py
@@ -1,11 +1,16 @@
 def plus(a, b):
     return a + b
 
-text = 'new text'
 
 def minus(a, b):
     return a - b
 
+
+def mul(a, b):
+    return a * b
+
+
 if __name__ == '__name__':
     print(plus(2, 2))
     print(plus(4, 2))
+    print(plus(3, 3))
